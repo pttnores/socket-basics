8ca02cc4ee7eea1637fe38eb8bbcb17f028440f8
José Nores <pttnores@gmail.com>
2017-06-26 13:40:52 -0300

    Send

--- public/js/app.js
+++ public/js/app.js
@@ -2,20 +2,22 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to the server');
-
-
-    socket.on('message', function (message) {
-        console.log('Message received: ' + message.text);
-        socket.broadcast.emit('message',{
-            text: 'Welcome to the chat application'
-        });
-    });
-
-    socket.emit('message',{
-        text: 'Welcome to the chat application'
-    });
 });
 
 socket.on('message', function (message) {
     console.log('New message: ' + message.text);
+});
+
+// Handle submitting
+var $form = jQuery('#message-form');
+
+$form.on('submit', function () {
+    event.preventDefault();
+
+    var message = $form.find('input[name=message]');
+
+    socket.emit('message',{
+        text: message.val()
+    });
+    message.val('');
 });
--- server.js
+++ server.js
@@ -8,9 +8,17 @@ app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
     console.log('User connected via socket.io!');
-    socket.emit('message',{
-        text: 'Welcome to the chat application'
+
+    socket.on('message', function (message) {
+        console.log('Message received: ' + message.text);
+
+        socket.broadcast.emit('message', message);
     });
+
+    socket.emit('message', {
+        text: 'Welcome to the chat application!'
+    });
+
 });
 
 http.listen(PORT, function () {
